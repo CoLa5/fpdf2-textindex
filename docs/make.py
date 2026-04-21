a7799aec3430aec09f2af6484aba9778924981bd
Corvin Lasogga <c.lasogga@gmail.com>
2026-04-21 23:43:18 +0200

    docs: Add icons to README

---
+++ docs/make.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Make docs."""
+
+import itertools
+import pathlib
+import shutil
+import textwrap
+from typing import Final
+
+import pdoc.render
+
+import fpdf2_textindex
+
+HERE: Final[pathlib.Path] = pathlib.Path(__file__).parent
+
+
+def write_sitemap(path: pathlib.Path) -> None:  # noqa: D103
+    with (path / "sitemap.xml").open("w", newline="\n") as f:
+        header = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+            """
+        f.write(textwrap.dedent(header).strip())
+        for file in path.glob("**/*.html"):
+            if file.name.startswith("_"):
+                continue
+            filename = (
+                file.relative_to(path).as_posix().replace("index.html", "")
+            )
+            f.write(
+                f"""\n<url><loc>https://cola5.github.io/fpdf2-textindex/{filename:s}</loc></url>"""
+            )
+        f.write("""\n</urlset>""")
+
+
+if __name__ == "__main__":
+    pdoc.render.configure(
+        docformat="google",
+        edit_url_map={
+            "fpdf2_textindex": "https://github.com/CoLa5/fpdf2-textindex/blob/main/fpdf2_textindex/",
+        },
+        favicon="/assets/favicon.svg",
+        footer_text=f"fpdf2_textindex v{fpdf2_textindex.__version__:s}",
+        logo="/assets/logo.svg",
+        logo_link="https://cola5.github.io/fpdf2-textindex/",
+        search=True,
+        template_directory=HERE / "pdoc_template",
+    )
+    output = HERE / ".." / "public"
+    output.mkdir(exist_ok=True)
+    pdoc.pdoc(
+        HERE / ".." / "fpdf2_textindex",
+        output_directory=output,
+    )
+    write_sitemap(output)
+
+    # Copy icons
+    (output / "assets").mkdir(exist_ok=True)
+    for name, suffix in itertools.product(
+        ("favicon", "logo"),
+        (".png", ".svg"),
+    ):
+        filename = name + suffix
+        shutil.copy2(
+            HERE / "icons" / filename,
+            output / "assets" / filename,
+        )
+    # Copy robots.txt
+    shutil.copy2(HERE / "robots.txt", output / "robots.txt")
