950265919b97f7057efb2d5bcda042ca192e3720
Corvin Lasogga <c.lasogga@gmail.com>
2026-04-23 11:57:55 +0200

    feat: Remove dependency on own fixed fpdf fork and instead monkeypatch the bugs here

---
+++ test/monkeypatch_test.py
@@ -0,0 +1,27 @@
+"""Test of :py:mod:`fpdf2`-monkey patch."""
+
+import sys
+
+
+def test_monkeypatch() -> None:
+    # Clean former imports
+    for name in list(sys.modules):
+        if name.startswith("fpdf"):
+            sys.modules.pop(name, None)
+
+    # Import original module
+    import fpdf
+
+    assert not hasattr(fpdf.FPDF, "__PATCHED__")
+    assert not hasattr(fpdf.fpdf.FPDF, "__PATCHED__")
+    assert not hasattr(fpdf.fpdf.MultiLineBreak, "__PATCHED__")
+
+    # Apply patch
+    import fpdf2_textindex  # noqa: F401
+
+    assert hasattr(fpdf.FPDF, "__PATCHED__") and fpdf.FPDF.__PATCHED__
+    assert hasattr(fpdf.fpdf.FPDF, "__PATCHED__") and fpdf.fpdf.FPDF.__PATCHED__
+    assert (
+        hasattr(fpdf.fpdf.MultiLineBreak, "__PATCHED__")
+        and fpdf.fpdf.MultiLineBreak.__PATCHED__
+    )
